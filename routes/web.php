b9f8936c2844adb1f20c537f708de3a4cedceb6e
Shithappe <greenmad.b50@gmail.com>
2023-09-16 22:12:38 +0300

    begin

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    
+    Route::get('/main', function () {
+        return Inertia::render('Main');
+    })->name('main');
 });
